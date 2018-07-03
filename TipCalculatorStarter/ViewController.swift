370a9dbde566cfce83641533a5ac0872e7a57613
calee14 <caplee1000@gmail.com>
2018-07-03 11:43:06 -0700

    now able to toggle switch

--- TipCalculatorStarter/ViewController.swift
+++ TipCalculatorStarter/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
     // MARK: - View Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        if sender.isOn {
+            print("switch is on")
+        } else {
+            print("switch is off")
+        }
+    }
+    
 }
 
